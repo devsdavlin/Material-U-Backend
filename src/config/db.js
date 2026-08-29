cd212c96a4297c1cbe17c9f5b36f8618434a24ab
devsdavlin <devsdavlin@gmail.com>
2026-08-28 22:28:45 -0500

    feat: configuracion inicial del servidor y conexion a PostgreSQL con pg

---
+++ src/config/db.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+const express = require('express');
+//
+const pool= new Pool({
+    connectionString: process.env.Data_Url,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+const app = express();
+//
+const conexionDb = async () => {
+    try {   
+        if (!process.env.Data_Url) {
+            throw new Error('La variable de entorno Data_Url no está definida.');
+        }
+        const Life= await pool.query('SELECT NOW()');
+        console.log('Conexión a la base de datos establecida:', Life.rows[0]);
+    }
+    catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        process.exit(1);
+    }
+}
+
+conexionDb();
+module.exports = pool;
